b8418c9076cb69f4fad1d464e077231f710cf7ec
Daniel F <dj.thd2@gmail.com>
2023-07-12 10:46:13 +0100

    Updated ConjuredItemUpdateService tests to match the requirement specs

--- go/services/conjured_item_update_service_test.go
+++ go/services/conjured_item_update_service_test.go
@@ -7,36 +7,36 @@ import (
     "github.com/emilybache/gildedrose-refactoring-kata/models"
 )
 
-// Conjured item quality decrements in 1 after each day
+// Conjured item quality decrements in 2 after each day
 func TestConjuredItemUpdateService_QualityBeforeSellIn(t *testing.T) {
     runTestCase(t, func(
         conjuredItemUpdateService ConjuredItemUpdateService,
     ) {
         item := models.NewItem(&models.ItemModel{"Conjured Mana Cake", 5, 5})
         conjuredItemUpdateService.UpdateQuality(item)
-        assert.Equal(t, 4, item.Model.Quality)
+        assert.Equal(t, 3, item.Model.Quality)
     })
 }
 
-// If the sellIn date has been passed, the Conjured item quality decrements in 2
+// If the sellIn date has been passed by 0 days, the Conjured item quality decrements in 4
 func TestConjuredItemUpdateService_QualityAfterSellIn0Days(t *testing.T) {
     runTestCase(t, func(
         conjuredItemUpdateService ConjuredItemUpdateService,
     ) {
         item := models.NewItem(&models.ItemModel{"Conjured Mana Cake", 0, 5})
         conjuredItemUpdateService.UpdateQuality(item)
-        assert.Equal(t, 3, item.Model.Quality)
+        assert.Equal(t, 1, item.Model.Quality)
     })
 }
 
-// If the sellIn date has been passed, the Conjured item quality decrements in 2
+// If the sellIn date has been passed (negative), the Conjured item quality decrements in 4
 func TestConjuredItemUpdateService_QualityAfterSellIn4Days(t *testing.T) {
     runTestCase(t, func(
         conjuredItemUpdateService ConjuredItemUpdateService,
     ) {
         item := models.NewItem(&models.ItemModel{"Conjured Mana Cake", -4, 5})
         conjuredItemUpdateService.UpdateQuality(item)
-        assert.Equal(t, 3, item.Model.Quality)
+        assert.Equal(t, 1, item.Model.Quality)
     })
 }
 
