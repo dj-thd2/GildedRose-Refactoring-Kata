98e09f564e8ac0e4a166c7cf604df66e0c89d542
Daniel F <dj.thd2@gmail.com>
2023-07-12 13:47:25 +0100

    Replaced UpdateQuality implementation with UpdateQualityForDays(1) on ConjuredItemUpdateService

--- go/services/conjured_item_update_service.go
+++ go/services/conjured_item_update_service.go
@@ -18,29 +18,7 @@ func NewConjuredItemUpdateService(logger lib.Logger, utils lib.Utils) ConjuredIt
 }
 
 func (this ConjuredItemUpdateService) UpdateQuality(item *models.Item) error {
-    item.Mutex.Lock()
-    defer item.Mutex.Unlock()
-
-    // The quality will decrement by 2 unit in normal conditions (double rate than normal)
-    decrement := 2
-
-    // If the item sellIn date is due, the quality will decrement at double rate
-    if item.Model.SellIn <= 0 {
-        decrement *= 2
-    }
-
-    // Decrement quality and force it to 0 if the result is negative
-    if (item.Model.Quality - decrement) < 0 {
-        item.Model.Quality = 0
-    } else {
-        item.Model.Quality -= decrement
-    }
-
-    // Decrement sellIn date
-    item.Model.SellIn--
-
-    // Return no errors
-    return nil
+    return this.UpdateQualityForDays(item, 1)
 }
 
 func (this ConjuredItemUpdateService) UpdateQualityForDays(item *models.Item, days int) error {
