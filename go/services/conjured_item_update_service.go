16d8c15d33907756a40f407f0647ba21799cec6a
Daniel F <dj.thd2@gmail.com>
2023-07-12 10:48:05 +0100

    Updated ConjuredItemUpdateService implementation to match the test specs

--- go/services/conjured_item_update_service.go
+++ go/services/conjured_item_update_service.go
@@ -16,20 +16,27 @@ func NewConjuredItemUpdateService(logger lib.Logger) ConjuredItemUpdateService {
 }
 
 func (this ConjuredItemUpdateService) UpdateQuality(item *models.Item) error {
-    decrement := 1
-
     item.Mutex.Lock()
     defer item.Mutex.Unlock()
 
+    // The quality will decrement by 2 unit in normal conditions (double rate than normal)
+    decrement := 2
+
+    // If the item sellIn date is due, the quality will decrement at double rate
     if item.Model.SellIn <= 0 {
         decrement *= 2
     }
+
+    // Decrement quality and force it to 0 if the result is negative
     if (item.Model.Quality - decrement) < 0 {
         item.Model.Quality = 0
     } else {
         item.Model.Quality -= decrement
     }
+
+    // Decrement sellIn date
     item.Model.SellIn--
 
+    // Return no errors
     return nil
 }
