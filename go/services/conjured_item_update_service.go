2847f4d557d828f7550f9a86b46d21424bd8df9c
Daniel F <dj.thd2@gmail.com>
2023-07-12 13:45:55 +0100

    Implemented ConjuredItemUpdateService -> UpdateQualityForDays and fix texttest use cases for ConjuredItem

--- go/services/conjured_item_update_service.go
+++ go/services/conjured_item_update_service.go
@@ -7,11 +7,13 @@ import (
 
 type ConjuredItemUpdateService struct {
     logger     lib.Logger
+    utils      lib.Utils
 }
 
-func NewConjuredItemUpdateService(logger lib.Logger) ConjuredItemUpdateService {
+func NewConjuredItemUpdateService(logger lib.Logger, utils lib.Utils) ConjuredItemUpdateService {
     return ConjuredItemUpdateService{
         logger:     logger,
+        utils:      utils,
     }
 }
 
@@ -42,9 +44,31 @@ func (this ConjuredItemUpdateService) UpdateQuality(item *models.Item) error {
 }
 
 func (this ConjuredItemUpdateService) UpdateQualityForDays(item *models.Item, days int) error {
-    for i := 0; i < days; i++ {
-        this.UpdateQuality(item)
+    item.Mutex.Lock()
+    defer item.Mutex.Unlock()
+
+    // The quality will decrement by 2 units in normal conditions for each day
+    normalDecrement := 2
+
+    // Calculate days to pass before SellIn date
+    passedDaysBeforeSellIn := this.utils.Max(this.utils.Min(days, item.Model.SellIn), 0)
+
+    // Calculate days to pass after SellIn date
+    passedDaysAfterSellIn := days - passedDaysBeforeSellIn
+
+    // Calculate total decrement
+    decrement := (normalDecrement * passedDaysBeforeSellIn) + (2 * normalDecrement * passedDaysAfterSellIn)
+
+    // Apply decrement to quality
+    if (item.Model.Quality - decrement) < 0 {
+        item.Model.Quality = 0
+    } else {
+        item.Model.Quality -= decrement
     }
 
+    // Decrement sellIn date
+    item.Model.SellIn -= days
+
+    // Return no errors
     return nil
 }
